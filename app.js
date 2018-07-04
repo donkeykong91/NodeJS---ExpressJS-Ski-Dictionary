5b27901d96a70ca60fad88f9f8813e1e5b71c4b0
donkeykong91 <darylnator@gmail.com>
2018-07-04 12:45:28 -0700

    Added get route for 'dictionary-api' which will respond with with json data of skier terms

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require("express");
-
 var app = express();
+
+var skierTerms = [
+    {
+        term: "Rip",
+        defined: "To move at a high rate of speed"
+    },
+    {
+        term: "Huck",
+        defined: "To throw your body off of something, usually a natural feature like a cliff"
+    },
+    {
+        term: "Chowder",
+        defined: "Powder after it has been sufficiently skied"
+    }
+];
 
 app.use(function (req, res, next) {
   console.log(`${req.method} request for '${req.url}'`);
@@ -8,6 +22,10 @@ app.use(function (req, res, next) {
 })
 
 app.use(express.static("./public"));
+
+app.get("/dictionary-api", function (req, res) {
+  res.json(skierTerms);
+});
 
 app.listen(3000);
 
